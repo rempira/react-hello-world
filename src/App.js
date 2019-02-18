9022b6fd9443eaef8da6cdd6687eaca41a416b71
Pierre Grimaud <grimaud.pierre@gmail.com>
2019-02-18 15:58:52 +0100

    Add deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Test extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
